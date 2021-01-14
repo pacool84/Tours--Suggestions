ba2ed767356fa3e632d276b1db8d0d78e16a5dc8
Francisco López <flopez_1384@hotmail.com>
2021-01-13 18:51:56 -0600

    [ADD] The toggle logic to show more or less info in the tour component

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./tours.css";
 
 function Tour({ id, image, info, price, name }) {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className='tour-article'>
       <img src={image} alt={name} />
@@ -9,7 +10,16 @@ function Tour({ id, image, info, price, name }) {
         <h3>{name}</h3>
         <h3>${price}</h3>
       </section>
-      <p className='tour-article__info'>{info}</p>
+      <p className='tour-article__info'>
+        {readMore ? info : info.substring(0, 300) + "..."}
+        <button
+          onClick={() => setReadMore(!readMore)}
+          className='tour-article__toggle'
+        >
+          {readMore ? "show less" : "showmore"}
+        </button>
+      </p>
+      <button>Not interested</button>
     </article>
   );
 }
